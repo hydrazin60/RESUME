04b2761873c222c2e5411bf97b5479013db23615
hydrazin60 <sureshkhdk45@gmail.com>
2024-05-31 21:27:09 +0545

    Install Tailwind CSS  and Flowbite React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from 'flowbite-react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 >Vite + React</h1>
+      <span className='text-3xl'> jiban pandey</span>
+      <Button>Click</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
